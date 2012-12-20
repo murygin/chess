796d244171f195099076412768ac7b9ceab3219c
Daniel Murygin <daniel.murygin@googlemail.com>
2012-12-20 20:18:37 +0100

    -Register form now accepts password with a '-'

--- src/main/java/org/n2/chess/beans/EmailValidator.java
+++ src/main/java/org/n2/chess/beans/EmailValidator.java
@@ -21,7 +21,7 @@ package org.n2.chess.beans;
  
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
- 
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -34,10 +34,7 @@ import org.springframework.stereotype.Component;
 @Component("emailValidator")
 public class EmailValidator implements Validator{
  
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\." +
-            "[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*" +
-            "(\\.[A-Za-z]{2,})$";
- 
+    private static final String EMAIL_PATTERN = "^([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]{2,})$";
     private Pattern pattern;
     private Matcher matcher;
     
